e47551df6e29be8edbd7386e857e246679e88416
jorgetejerasoto <jorgetejerasoto@alumno.ieselrincon.es>
2024-09-25 21:13:31 +0100

    mejora de registro de dudas

--- registra_dudas.php
+++ registra_dudas.php
@@ -3,10 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Result</title>
 </head>
 <body>
-    <h1>FUNCIONA</h1>
+    <div class="resp">
+        <h1>Completado</h1>
+        <p>Su duda se ha registrado con éxito. Pronto tendrá su respuesta.</p>
+        <a href="./index.html"><button>Nueva duda</button></a>
+    </div>
+    
     <?php
         $correo = $_POST["correo"];
         $modulo= $_POST["modulo"];
