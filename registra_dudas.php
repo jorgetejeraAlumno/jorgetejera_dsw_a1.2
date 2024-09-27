263025236cc533addbbd59ea478d5d31abecb1a2
jorgetejeraAlumno <jorgetejerasoto@alumno.ieselrincon.es>
2024-09-27 11:50:49 +0100

    Se termina la v2.0

--- registra_dudas.php
+++ registra_dudas.php
@@ -8,46 +8,54 @@
 </head>
 <body>
     
-    
+<div class="resp">
     <?php
     $resp1="";
     $resp2="";
-    $DAW2=["DSW","DEW","EMR","DPL","DOR"];
+    $daw2=array("DSW","DEW","EMR","DPL","DOR");
     if ($_SERVER["REQUEST_METHOD"]==="POST") 
         {
             $errores = [];
             //Validamos el correo
             if(empty($_POST["correo"])) {
-                $errores[] = "Este campo es obligatorio";
+                $errores[] = "El correo es obligatorio";
             }elseif(!filter_var($_POST["correo"],FILTER_VALIDATE_EMAIL)){
                 $errores[] = "Este mail es inválido";
             }
             //Validamos el asunto
             if(empty($_POST["asunto"])) {
-                $errores[] = "Este campo es obligatorio";
-            }elseif(!is_string($_POST["asunto"]))
+                $errores[] = "El asunto es obligatorio";
+            }elseif(is_numeric($_POST["asunto"]))
             {
                 $errores[] = "El valor de este campo debe ser texto";
             }elseif(strlen($_POST["asunto"])>50) {
-                $errores[] = "El asunto es de maximo 50 caracteres";
+                $errores[] = "El asunto es de maximo 50 caracteres. Usted ha puesto: ".strlen($_POST["asunto"]);
             }
             // Validamos la describcion
             if(empty($_POST["desc"])) {
-                $errores[] = "Este campo es obligatorio";
+                $errores[] = "La descripcion es obligatorio";
             }elseif(strlen($_POST["desc"])> 300) {
-                $errores[] = "Puedes poner como maximo 300 caracteres";
+                $errores[] = "El maximo de caracteres de la descripcion es de 300. Usted ha puesto: ".strlen($_POST["desc"]);
             }
-            //Comprobamos si hat errores
+
+            //Comprobamos que el asunto pertenece al array DAW2
+            if(!in_array($_POST["modulo"], $daw2)) {
+                $errores[] = "La duda debe ser sobre una asignatura de segundo. ".$_POST["modulo"]." es de primero";
+            }
+            //Comprobamos si hay errores
             if(empty($errores)){
                 $resp1="Completado";
                 $resp2="Su duda se ha registrado con exito";
+
                 //Guardamos en variables los campos
                 $correo = $_POST["correo"];
                 $modulo= $_POST["modulo"];
                 $asunto= $_POST["asunto"];
                 $desc= $_POST["desc"];
+
                 //Los guardamos en un array
                 $duda ="\"$correo\";\"$modulo\";\"$asunto\";\"$desc\"\n";
+
                 //Lo escribimos en el csv
                 $file = fopen("dudas.csv","a+");
                 fwrite($file,$duda);
@@ -55,16 +63,16 @@
                 echo"<h1>".$resp1."</h1>";
                 echo "<p>".$resp2."</p>";
             }else{
-                $resp1="Errores:";
+                $resp1="<h2>"."Errores:"."</h2>";
                 echo $resp1;
-
                 foreach($errores as $error) {
-                    echo "<br>".$error."<br>";
+                    echo"<br>".$error."<br>";
                 }
+                
             }
         }
     ?>
-    <div class="resp">
+    
         <a href="./index.html"><button>Nueva duda</button></a>
     </div>
      
